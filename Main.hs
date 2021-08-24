f232086ce81164143cf07262dc2f554fe951666c
d0ku <darthtyranus666666@gmail.com>
2021-08-24 22:53:59 +0200

    whereify a bit + prepare Grav to be split into multiple Object instances

--- Main.hs
+++ Main.hs
@@ -4,24 +4,24 @@ import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Data.Fixed (mod')
 
-gravConstant :: Float
-gravConstant = 6.674 ** (-11)
-
 data Grav = Grav
     { moonLoc :: (Float, Float) 
     , moonDeg :: Float
     , moonRad :: Float
     , moonMass :: Float
+    , moonColor :: Color
 
     , planetLoc :: (Float, Float)
     , planetVel :: (Float, Float)
     , planetRad :: Float
     , planetMass :: Float
+    , planetColor :: Color
 
     , shipLoc :: (Float, Float)
     , shipVel :: (Float, Float)
     , shipRad :: Float
     , shipMass :: Float
+    , shipColor :: Color
 } deriving (Show)
 
 startState :: Grav
@@ -30,34 +30,35 @@ startState = Grav
     , moonDeg = 0
     , moonRad = 7
     , moonMass = 1000
+    , moonColor = light $ light blue
 
     , planetLoc = (10, 10)
     , planetVel = (0,0)
     , planetRad = 22
     , planetMass = 10000000000
+    , planetColor = dark green
     
     , shipLoc = (100, 60)
     , shipVel = (0,0.2)
     , shipRad = 3
     , shipMass = 10000000
+    , shipColor = red
     }
 
 render :: Grav -> Picture
 render game = 
     pictures [planet, ship, moon]
     where
-    planet = uncurry translate (planetLoc game) $ color planetColor $ circleSolid (planetRad game)
-    ship = uncurry translate (shipLoc game) $ color shipColor $ circleSolid (shipRad game)
-    moon = uncurry translate (moonLoc game) $ color moonColor $ circleSolid (moonRad game)
-    shipColor = red
-    planetColor = dark green
-    moonColor = light $ light blue
+    planet = uncurry translate (planetLoc game) $ color (planetColor game) $ circleSolid (planetRad game)
+    ship = uncurry translate (shipLoc game) $ color (shipColor game) $ circleSolid (shipRad game)
+    moon = uncurry translate (moonLoc game) $ color (moonColor game) $ circleSolid (moonRad game)
 
 move :: (Float, Float) -> (Float, Float) -> (Float, Float)
 move (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 gravityForce :: Float -> Float -> Float -> Float
-gravityForce massOne massTwo radius = (gravConstant * massOne * massTwo) / (radius ^ 2)
+gravityForce massOne massTwo radius = (gCons * massOne * massTwo) / (radius ^ 2)
+  where gCons = 6.674 ** (-11)
 
 calculateSpeed :: (Float, Float) -> (Float, Float) -> Float -> (Float, Float)
 calculateSpeed (xSpeed, ySpeed) (xDiff, yDiff) speed = (xSpeed + xDiffC, ySpeed + yDiffC)
